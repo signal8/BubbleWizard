1b7702fb4e765319307d7fae565407b7a082d780
signal8 <irrelevant8590@gmail.com>
2025-01-25 11:41:18 -0500

    Add Enemy Types

--- Scenes/enemy.gd
+++ Scenes/enemy.gd
@@ -11,21 +11,46 @@ enum state {
 	DEAD
 }
 
+enum type {
+	GREEN,
+	BROWN,
+	RED,
+	PURPLE
+}
+
 @export var currentState : state
+@export var currentType : type
+
+@export var greenTexture : Texture2D
+@export var brownTexture : Texture2D
+@export var redTexture : Texture2D
+@export var purpleTexture : Texture2D
+
+func _ready() -> void:
+	match currentType:
+		0: #Green
+			sprite.texture = greenTexture
+		1: #Brown
+			sprite.texture = brownTexture
+		2: #Red
+			sprite.texture = redTexture
+		3: #purple
+			sprite.texture = purpleTexture
 
 func _process(delta: float) -> void:
 	match currentState:
 		0: #Roaming
 			pass
 		1: #Captured
-			rotation += 3000
+			sprite.rotation += 1 * delta
 		2: #Dead
 			pass
+	
 
 func capture():
 	if currentState == 1: return
 	
-	sprite.scale = Vector2(1,0.5)
+	sprite.scale = Vector2(0.5,0.5)
 	bubbleSprite.visible = true
 	collision.set_deferred("disabled", true)
 	await get_tree().process_frame
