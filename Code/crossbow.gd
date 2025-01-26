c92ed903628fca439f81ff3925e4716d8a6f91cf
signal8 <irrelevant8590@gmail.com>
2025-01-26 13:21:53 -0500

    arrows + spikes

--- Code/crossbow.gd
+++ Code/crossbow.gd
@@ -1,10 +1,16 @@
 extends Area2D
 
 var gunked = false
+@onready var timer = $Timer
+@export var arrow : PackedScene
 
 func _process(delta: float) -> void:
 	if gunked: return
-	pass
+	elif timer.is_stopped():
+		var a = arrow.instantiate()
+		add_child(a)
+		timer.start(1)
+		
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("gum"):
